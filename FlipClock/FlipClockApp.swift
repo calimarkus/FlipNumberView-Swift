d556941c9e78a66c2f85d8acdad8ca098e3958d3
Markus Emrich <Markus.Emrich@gmail.com>
2022-05-22 10:52:59 +0900

    simplify filestructure / use @main

---
+++ FlipClock/FlipClockApp.swift
@@ -0,0 +1,13 @@
+//
+//
+
+import SwiftUI
+
+@main
+struct FlipClockApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
--- FlipClock/Views/ContentView.swift
+++ FlipClock/Views/ContentView.swift
@@ -9,3 +9,10 @@ struct ContentView: View {
     }
 
 }
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+
