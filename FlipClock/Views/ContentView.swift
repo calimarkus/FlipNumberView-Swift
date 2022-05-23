f0e603668894fc1fb8d914fd0649adc0cbc261bc
Markus Emrich <Markus.Emrich@gmail.com>
2022-05-23 11:23:35 +0900

    add labels

--- FlipClock/Views/ContentView.swift
+++ FlipClock/Views/ContentView.swift
@@ -2,10 +2,17 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack(spacing: 50.0) {
+        VStack(spacing: 10.0) {
+            Spacer()
+
+            Text("Text based:").foregroundColor(.black)
             ClockView(config: FlipTextViewConfig(fontSize: 50.0))
+            Spacer().frame(height: 10.0)
+            Text("Image based:").foregroundColor(.black)
             ClockView(viewModel: ClockViewModel(animationDuration: 0.66),
-                isTextBased: false, imageScale: 0.5)
+                isTextBased: false, imageScale: 0.6)
+
+            Spacer()
         }
     }
 }
